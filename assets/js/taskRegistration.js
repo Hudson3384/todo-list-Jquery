138119d5fd6b15bf3a47a98456109641b67d353c
hudson3384 <hudson.arruda@outlook.com>
2022-10-02 00:52:43 -0300

    :sparkles: Add tasks in localStorage

--- assets/js/taskRegistration.js
+++ assets/js/taskRegistration.js
@@ -1,12 +1,46 @@
 $(document).ready(function () {
   $("#form-task").submit(function (e) {
-    let task = $("#taskInput").val()
-    let date = $("#date").val()
+    let task = $("#taskInput").val();
+    let date = $("#date").val();
 
     if (!task || !date) {
-        return 
+      $("#alert-register-error").show();
+      return false;
     }
-    console.log(task, date)
+
+    saveTaskLocalStorage(task, date);
+
     e.preventDefault();
   });
+
+  $(".btn-close").click(function () {
+    $(this).parent().css("display", "none");
+  });
+
+  $("#date").mask("00/00/0000");
 });
+
+function saveTaskLocalStorage(task, date) {
+  
+  let  obj = {
+    id: new Date().getTime(),
+    task,
+    date,
+  };
+
+  let savedTasks = JSON.parse(getTasksLocalStorage());
+ 
+  if (savedTasks.length) {
+    savedTasks.push(obj);
+
+    localStorage.setItem("tasks-todo-list", JSON.stringify(savedTasks));
+    
+  } else {
+    localStorage.setItem("tasks-todo-list", JSON.stringify([obj]));
+  }
+}
+
+function getTasksLocalStorage() {
+  
+  return localStorage.getItem("tasks-todo-list");
+}
